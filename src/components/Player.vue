fc432d5d878c27038a923d7feb3efe513c8cf897
Guilherme Henrique <guilhermeerba@hotmail.com>
2020-02-26 12:29:39 -0300

    first commit

---
+++ src/components/Player.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <div id="audio" class="player-wrapper">
+      <audio-player></audio-player>
+    </div>
+  </div>
+</template>
+
+<script>
+import AudioPlayer from "./AudioPlayer.vue";
+
+export default {
+  name: "Player",
+  components: {
+    AudioPlayer
+  }
+  //https://upload.wikimedia.org/wikipedia/commons/d/d6/Louis-Emmanuel_Jadin_-_Nocturne_No._3_in_G_minor_-_2._Allegro_molto.ogg
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss"></style>
